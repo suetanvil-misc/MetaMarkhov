dcf963c5d2cdbe49ddbf8031c95e36207fb678ee
Chris Reuter <chris@blit.ca>
2013-04-02 20:53:11 -0400

    Builder renamed to MCBuilder; now accepts input.

--- builder.rb
+++ builder.rb
@@ -1,13 +1,46 @@
 
 # Builder class skeleton
 
-class Builder
+class WordEntry
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+    @counts = {}
+  end
+
+  def follow(word)
+    @counts[word] = 0 unless @counts.has_key?(word)
+    @counts[word] += 1
+  end
+end
+
+
+class MCBuilder
+  def initialize()
+    @words = {}
+    @lastWord = nil
+  end
+
   def addWord(theWord)
-    puts "Adding word '#{theWord}'"
+    @lastWord.follow(theWord) if @lastWord
+    @lastWord = entryFor(theWord)
   end
 
   def extrude(filename)
     filename += '.rb' unless filename.end_with?('.rb')
     puts "Extruding to '#{filename}'"
   end
+
+  private
+
+  def addFirstWord(theWord)
+    @lastWord = entryFor(theWord)
+  end
+
+  def entryFor(theWord)
+    @words[theWord] = WordEntry.new(theWord) unless @words.has_key?(theWord)
+    return @words[theWord]
+  end
 end
+
