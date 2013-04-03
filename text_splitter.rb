bcbd2e0386580c157f3fbf881f23c1facf75fffb
ashbt <ashley.beattie@gmail.com>
2013-04-03 08:15:59 -0400

    Ashley's first commit, early work -- created the text splitter, still figuring out how to do conditional regular expressions.

---
+++ text_splitter.rb
@@ -0,0 +1,16 @@
+=begin
+
+- take a text document
+-- somehow contextualize the document
+-- build a long string of text, divided or tagged according 
+-- 
+
+
+-- no whitespace
+-- split on the basis of punctuation in general
+
+the class:
+- arg parser
+- output file name from the argument list
+  
+=end
